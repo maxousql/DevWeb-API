1d1a416b9dd58269694dea83cae659c848ece5af
maxousql <maxlaiyio@outlook.fr>
2023-03-09 16:57:14 +0100

    exercice sur les tableaux

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const maVariable = 'Hello world!'; // Ceci est un commentaire
+console.log(maVariable);
+/*
+ * 
+ * Ceci est un commenatire multi ligne
+ * 
+ */
+let nom = "LAI YIO LAI TONG";
+let prenom = "Maxime";
+console.log(nom + " " + prenom);
+
+console.log(nom.concat(" ", prenom));
